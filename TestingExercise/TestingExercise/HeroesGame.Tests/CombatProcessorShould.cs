10430732a51921bd5e994bc18e669105794ac0d6
ZdrDimitrova <dimitrova@pmgkk.com>
2021-04-04 14:30:20 +0300

    Testing

---
+++ TestingExercise/TestingExercise/HeroesGame.Tests/CombatProcessorShould.cs
@@ -0,0 +1,33 @@
+﻿using HeroesGame.Contract;
+using HeroesGame.Implementation.Hero;
+using HeroesGame.Implementation.Monster;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HeroesGame.Tests
+{
+    public class CombatProcessorShould
+    {
+        private CombatProcessor _cp;
+
+        [SetUp]
+        public void Setup()
+        {
+            this._cp = new CombatProcessor(new Hunter());
+           
+        }
+
+        [Test]
+        public void InitializeCorrectly()
+        {
+            Assert.That(this._cp.Hero, Is.Not.Null);
+            Assert.That(this._cp.Logger, Is.Not.Null);
+            Assert.That(this._cp.Logger, Is.Empty);
+        }
+
+       
+
+    }
+}
